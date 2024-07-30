6979c42e30c620260941a0ae6daf97fadb86dfad
john1828 <jeleyson@yahoo.com>
2024-07-30 18:28:06 +0800

    Update FeaturedProducts.jsx

--- src/components/FeaturedProducts.jsx
+++ src/components/FeaturedProducts.jsx
@@ -1,7 +1,6 @@
 import { useState, useEffect } from "react";
 import { CardGroup } from "react-bootstrap";
 import PreviewProducts from "./PreviewProducts";
-import "../index.css";
 
 export default function FeaturedProducts() {
   const [previews, setPreviews] = useState([]);
